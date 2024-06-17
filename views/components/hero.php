ae6f4414013287772d43c9e4b8f5d127f29b62cf
LaylaTD <layla.tamb@gmail.com>
2024-06-17 14:27:23 -0400

    Hero mobile design

--- views/components/hero.php
+++ views/components/hero.php
@@ -1,5 +1,11 @@
 <section class="hero">
-    <h1>Tire-toi une bûche!</h1>
-    <p>Bienvenue au Pub G6, un établissement qui séduit les gourmets depuis <span class="bold">20 ans</span> 🍺</p>
-    <a href="" class="btn">Voir notre menu</a>
+    <div>
+        <h1>Tire-toi une bûche!</h1>
+        <p>Bienvenue au Pub G6, un établissement qui séduit les gourmets depuis <span class="bold">20 ans</span> 🍺</p>
+        <a href="" class="btn">Voir notre menu</a>
+    </div>
+
+    <div class="img-hero">
+        <img src="" alt="">
+    </div>
 </section>
