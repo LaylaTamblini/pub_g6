f8412d589b6454ec59da189fdf95f02554cb0106
LaylaTamblini <layla.tamb@gmail.com>
2024-06-17 09:09:06 -0400

    Ajout fichiers scss

---
+++ views/components/header.php
@@ -0,0 +1,14 @@
+<header>
+    <nav>
+        Logo
+    </nav>
+
+    <nav>
+        <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">À propos</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</header>
