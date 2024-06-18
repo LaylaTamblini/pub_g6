92706d9bb500a120711eadb6b1474f0b5ca4588c
LaylaTD <layla.tamb@gmail.com>
2024-06-18 15:21:13 -0400

    Section commentaire desktop + mobile

--- views/components/commentaires.php
+++ views/components/commentaires.php
@@ -2,7 +2,7 @@
     <div class="overlay"></div>
 
     <div class="content">
-        <p class="commentaire-client">"Pub G6 est ma destination favorite lorsque je veux recevoir des invités. Fiables et toujours une ambiance très agréable."</p>
+        <p class="commentaire-client">" Pub G6 est ma destination favorite lorsque je veux recevoir des invités. Fiables et toujours une ambiance très agréable. "</p>
     
         <div>
             <span class="material-icons">
