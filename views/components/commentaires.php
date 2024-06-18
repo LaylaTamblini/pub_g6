dc514da28d3e84cf7cd18c91f433a88aa817266c
LaylaTD <layla.tamb@gmail.com>
2024-06-18 14:44:37 -0400

    section commentaires overlay

--- views/components/commentaires.php
+++ views/components/commentaires.php
@@ -1,11 +1,15 @@
 <section class="commentaires">
-    <p class="commentaire-client">"Pub G6 est ma destination favorite lorsque je veux recevoir des invités. Fiables et toujours une ambiance très agréable."</p>
+    <div class="overlay"></div>
 
-    <div>
-        <span class="material-icons">
-            star
-        </span>
+    <div class="content">
+        <p class="commentaire-client">"Pub G6 est ma destination favorite lorsque je veux recevoir des invités. Fiables et toujours une ambiance très agréable."</p>
+    
+        <div>
+            <span class="material-icons">
+                star
+            </span>
+        </div>
+    
+        <p class="nom-client">- Nom du client</p>
     </div>
-
-    <p class="nom-client">- Nom du client</p>
 </section>
