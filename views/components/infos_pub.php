4448a55481e8229fddabd480cc24f8cb1dded5af
LaylaTD <layla.tamb@gmail.com>
2024-06-18 13:15:02 -0400

    section infos pub mobile

--- views/components/infos_pub.php
+++ views/components/infos_pub.php
@@ -1,11 +1,11 @@
 <section class="infos-pub">
     <div>
-        <img src="" alt="">
-        <p></p>
+        <img src="public/img/infos_pub1.jpg" alt="">
+        <p>Chaque plat est préparé avec <span class="bold">des ingrédients frais et locaux</span>, reflétant notre engagement envers la qualité et l'authenticité.</p>
     </div>
     
-    <div>
-        <img src="" alt="">
-        <p></p>
+    <div class="no-pad">
+        <img src="public/img/infos_pub2.jpg" alt="">
+        <p>En célébrant <span class="bold">deux décennies</span> de succès, nous continuons à innover et à réinventer nos menus pour surprendre et ravir nos clients fidèles.</p>
     </div>
 </section>
