7993c7b24e9b10a23ea5d7d23369aab06b60d140
LaylaTamblini <layla.tamb@gmail.com>
2024-06-19 09:21:55 -0400

    infolettre desktop + mobile

---
+++ views/components/infolettre.php
@@ -0,0 +1,16 @@
+<section class="infolettre">
+    <div>
+        <h3><span>Ne ratez</span> aucune miette</h3>
+        <p>Abonnez-vous à notre infolettre 📫</p>
+    </div>
+
+    <form action="">
+        <div class="inputs">
+            <input type="text" name="prenom" placeholder="Prénom">
+            <input type="text" name="nom" placeholder="Nom">
+            <input type="email" name="courriel" placeholder="Adresse courriel">
+        </div>
+
+        <input type="submit" value="Abonnez-vous">
+    </form>
+</section>
--- views/index.view.php
+++ views/index.view.php
@@ -9,6 +9,7 @@
     <?php include("views/components/infos_pub.php") ?>
     <?php include("views/components/commentaires.php") ?>
     <?php include("views/components/contact.php") ?>
+    <?php include("views/components/infolettre.php") ?>
 </main>
 
 <?php include("views/components/foot.php") ?>
