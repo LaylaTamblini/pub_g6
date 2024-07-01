fb1e81d959023ad46aacfa8f7e0381e731447828
LaylaTamblini <layla.tamb@gmail.com>
2024-07-01 16:45:44 -0400

    section 1200px

--- views/components/about.php
+++ views/components/about.php
@@ -1,10 +1,10 @@
 <section class="about" id="about">
     <div>
-        <h2><span class="block">À propos</span> de nous</h2>
-        <p>Niché au cœur de la ville, le Pub G6 est devenu un lieu de rencontre incontournable pour les amoureux de la bonne cuisine 🥰</p>
-    </div>
-    
-    <div class="about-img">
+        <section class="about-title">
+            <h2><span class="block">À propos</span> de nous</h2>
+            <p>Niché au cœur de la ville, le Pub G6 est devenu un lieu de rencontre incontournable pour les amoureux de la bonne cuisine 🥰</p>
+        </section>
+        
         <img loading="lazy" src="public/img/pub_apropos.jpg" alt="Bâtiment rustique du Pub G6 avec une terrasse et des parasols jaunes">
     </div>
 </section>
